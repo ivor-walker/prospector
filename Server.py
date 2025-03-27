0d6d66ebc496a645c1d2fe3569c1fb49661a95d4
latb1 <latb1@pc7-111-l.cs.st-andrews.ac.uk>
2025-03-27 19:53:08 +0000

    moved server

--- Server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# Dummy Server
-class Server:
-    def __init__(self):
-        self.gamesList = ["lobbyOne", "northLobbyIsBestLobby", "baseTradeLobby", "ProspectorLloby"]
-
-    def getGamesList(self):
-        return self.gamesList
